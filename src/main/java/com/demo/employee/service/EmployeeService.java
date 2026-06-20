7fa59186bbfbe3a2233f4a17cd4d51d8256a5451
javad86 <javad86@gmail.com>
2026-06-20 13:33:26 -0400

    added logic for saving and updating employee

--- src/main/java/com/demo/employee/service/EmployeeService.java
+++ src/main/java/com/demo/employee/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.demo.employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,21 @@ public class EmployeeService {
         //Employee emp = new Employee("123", "John", "Doe", 25);
         Optional<Employee> employeeOptional = repo.findById(id);
         return employeeOptional.orElse(null);
+    }
+
+    public void addEmployee(Employee employee) {
+        repo.save(employee);
+    }
+
+    public void updateEmployee(Employee employee) {
+        repo.save(employee);
+    }
+
+    public void removeEmployee(Employee employee) {
+        repo.delete(employee);
+    }
+
+    public List<Employee> getAllEmployee() {
+        return repo.findAll();
     }
 }
